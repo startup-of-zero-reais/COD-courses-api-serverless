f5c0c3c7ac20d1a1639fa2453786c47eeec84296
Jean <jean.carlosmolossi@gmail.com>
2022-01-04 01:19:39 -0300

    chore: atualizado schema da tabela

--- terraform/dynamodb/main.tf
+++ terraform/dynamodb/main.tf
@@ -16,14 +16,17 @@ resource "aws_dynamodb_table" "this" {
   read_capacity  = 5
   write_capacity = 5
 
-  hash_key  = "ID"
-  range_key = "ParentID"
+  hash_key  = "PK"
+  range_key = "SK"
 
   dynamic "attribute" {
     for_each = {
-      ID        = "S"
-      ParentID  = "S"
-      CreatedAt = "N"
+      PK           = "S"
+      SK           = "S"
+      Owner        = "S"
+      Title        = "S"
+      ParentCourse = "S"
+      ParentModule = "S"
     }
 
     content {
@@ -32,16 +35,49 @@ resource "aws_dynamodb_table" "this" {
     }
   }
 
-  global_secondary_index {
-    name               = "ParentIDIndex"
-    hash_key           = "ParentID"
-    projection_type    = "INCLUDE"
-    non_key_attributes = [
-      "ID"
-    ]
-    range_key          = "CreatedAt"
-    read_capacity      = 5
-    write_capacity     = 5
+  dynamic "global_secondary_index" {
+    for_each = {
+      CourseOwnerIndex = {
+        hash_key        = "SK"
+        range_key       = "Owner"
+        projection_type = "ALL"
+        read_capacity   = 5
+        write_capacity  = 5
+      }
+
+      CourseTitleIndex = {
+        hash_key        = "Title"
+        range_key       = "SK"
+        projection_type = "ALL"
+        read_capacity   = 5
+        write_capacity  = 5
+      }
+
+      ModuleLessonsIndex = {
+        hash_key        = "ParentModule"
+        range_key       = "SK"
+        projection_type = "ALL"
+        read_capacity   = 5
+        write_capacity  = 5
+      }
+
+      CourseLessonsIndex = {
+        hash_key        = "ParentCourse"
+        range_key       = "SK"
+        projection_type = "ALL"
+        read_capacity   = 5
+        write_capacity  = 5
+      }
+    }
+
+    content {
+      name            = global_secondary_index.key
+      hash_key        = lookup(global_secondary_index.value, "hash_key")
+      range_key       = lookup(global_secondary_index.value, "range_key")
+      projection_type = lookup(global_secondary_index.value, "projection_type")
+      read_capacity   = lookup(global_secondary_index.value, "read_capacity")
+      write_capacity  = lookup(global_secondary_index.value, "write_capacity")
+    }
   }
 
   tags = local.common_tags
