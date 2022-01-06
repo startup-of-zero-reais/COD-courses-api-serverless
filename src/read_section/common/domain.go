ffab88e79cc5df4864ef7a09209ef4bfbb076412
Jean <jean.carlosmolossi@gmail.com>
2022-01-06 00:03:12 -0300

    feat: implementadas rotas de busca

--- src/read_section/common/domain.go
+++ src/read_section/common/domain.go
@@ -1,17 +1,11 @@
 package common
 
 type (
-	Lesson struct {
-		SK            string            `json:"lesson_id"`
-		PK            string            `json:"section_id"`
-		Title         string            `json:"title"`
-		Thumb         string            `json:"thumb"`
-		VideoSource   string            `json:"video_source"`
-		DurationTotal uint              `json:"duration_total"`
-		ParentCourse  string            `json:"parent_course"`
-		ParentModule  string            `json:"parent_module"`
-		Artifacts     map[string]string `json:"artifacts"`
-		CreatedAt     int64             `json:"created_at"`
-		UpdatedAt     int64             `json:"updated_at"`
+	Section struct {
+		PK        string `json:"module_id"`
+		SK        string `json:"section_id"`
+		Title     string `json:"title"`
+		CreatedAt int64  `json:"created_at"`
+		UpdatedAt int64  `json:"updated_at"`
 	}
 )
--- src/read_section/main.go
+++ src/read_section/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "log"
+import (
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"log"
+	"read_section/common"
+)
+
+type (
+	Handler struct {
+		dynamoClient *dynamodb.Client
+	}
+)
 
 func main() {
-	log.Println("Hello read section")
+	dynamoClient, err := common.DynamoConnection()
+	if err != nil {
+		log.Fatalf("failed to load config, %v", err)
+	}
+
+	h := Handler{
+		dynamoClient: dynamoClient,
+	}
+
+	lambda.Start(h.HandleLambda)
+}
+
+func (h Handler) HandleLambda(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	if request.QueryStringParameters["action"] != "search" {
+		return h.Get(request)
+	}
+
+	return h.Search(request)
+}
+
+func (h Handler) Get(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return common.ResponseProxy(200, common.NewDataResponse("Get Request"), nil)
+}
+
+func (h Handler) Search(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	return common.ResponseProxy(200, common.NewDataResponse("Search Request"), nil)
 }
