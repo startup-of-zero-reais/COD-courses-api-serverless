d954c74408b21e2874009d2eed2ff58f7cbd5acb
Jean <jean.carlosmolossi@gmail.com>
2022-01-05 23:26:53 -0300

    feat: inicializado o common da lambda

---
+++ src/read_section/common/utils.go
@@ -0,0 +1,33 @@
+package common
+
+import (
+	"encoding/json"
+	"github.com/aws/aws-lambda-go/events"
+)
+
+type (
+	Stringify interface {
+		String() string
+	}
+)
+
+func ToString(s interface{}) string {
+	bytes, _ := json.Marshal(s)
+	return string(bytes)
+}
+
+func Json(s Stringify) string {
+	if s == nil {
+		return ""
+	}
+
+	return s.String()
+}
+
+func ResponseProxy(statusCode int, body Stringify, err error) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode:      statusCode,
+		Body:            Json(body),
+		IsBase64Encoded: false,
+	}, err
+}
