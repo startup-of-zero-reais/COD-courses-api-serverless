fe16bb4057af14ff354090e72248e1a14e56f908
Jean <jean.carlosmolossi@gmail.com>
2022-01-08 14:39:56 -0300

    refactor: ajustado sectionorders model

--- src/read_module/common/domain.go
+++ src/read_module/common/domain.go
@@ -1,13 +1,14 @@
 package common
 
 type (
-	Module struct {
-		PK            string   `json:"course_id"`
-		SK            string   `json:"module_id"`
-		Title         string   `json:"title"`
-		SectionsOrder []string `json:"sections_order"`
-		UnlockAfter   uint     `json:"unlock_after"`
-		CreatedAt     int64    `json:"created_at"`
-		UpdatedAt     int64    `json:"updated_at"`
+	SectionsOrder map[string]string
+	Module        struct {
+		PK            string        `json:"course_id"`
+		SK            string        `json:"module_id"`
+		Title         string        `json:"title"`
+		SectionsOrder SectionsOrder `json:"sections_order"`
+		UnlockAfter   uint          `json:"unlock_after"`
+		CreatedAt     int64         `json:"created_at"`
+		UpdatedAt     int64         `json:"updated_at"`
 	}
 )
